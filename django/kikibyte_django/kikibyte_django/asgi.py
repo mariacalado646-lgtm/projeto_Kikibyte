2dd16f02f16d15f1419e7c7bb463e488dd7df31f
itito <elititoog@gmail.com>
2026-05-18 11:03:57 +0100

    Recriado projeto Django para usar venvs do python para evitar problemas com dependencias

--- django/kikibyte_django/kikibyte_django/asgi.py
+++ django/kikibyte_django/kikibyte_django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for cybersec_platform project.
+ASGI config for kikibyte_django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cybersec_platform.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kikibyte_django.settings")
 
 application = get_asgi_application()
--- django/kikibyte_django/kikibyte_django/wsgi.py
+++ django/kikibyte_django/kikibyte_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cybersec_platform project.
+WSGI config for kikibyte_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cybersec_platform.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kikibyte_django.settings")
 
 application = get_wsgi_application()
